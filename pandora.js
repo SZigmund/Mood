eb96a0afa66143d4f8000a7139a33db57d4f1550
SZigmund <szigmund@hotmail.com>
2023-08-28 16:21:57 -0400

    Update pandora.js

--- pandora.js
+++ pandora.js
@@ -2,7 +2,7 @@ var STARTUP = {
   initpage: function(){
 	try {
 		document.querySelector("#username").value="admin";
-		console.log(document.querySelector("#username")).value;
+		document.querySelector("#password").value="1311931";
 	}
 	catch (err) { console.log("initpage: " + err.message); }
   },
